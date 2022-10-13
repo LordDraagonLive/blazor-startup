989534551daa1fa22c894bc058e51ba0e9166487
Buddhi Adhikari <buddhiadikari@live.com>
2022-10-13 20:07:00 +0530

    User models

---
+++ BlazorServerApp/Data/User.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorServerApp.Data
+{
+    public class User : IUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public string? Country { get; set; } = null;
+        public string? Phone { get; set; }
+
+        public User()
+        {
+
+        }
+
+    }
+}
